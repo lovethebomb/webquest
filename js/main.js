7a87b2be88dbac2a09a5fb9d2a0e814fca87ce3e
Lucas <lucas@lovethebomb.eu>
2012-11-06 01:08:16 +0100

    Ajout Pixel Sense + Epawn + Correctifs drag n drop

--- js/main.js
+++ js/main.js
@@ -15,8 +15,92 @@
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1700);
     });
 
+    /* EPawn */
+    var i = 0;
+    var colors = new Array('jaune', 'red', 'vert', 'bleu');
+    $("div#epawn div").click(function() {
+    	$(this).parent().css('background-image','url("img/commercialisation/_epawn_'+ colors[i] +'.png")');
+    	if(i == colors.length-1) 
+    		i = 0;
+    	else 
+    		i++;
+    });
 
-    /* Drag and Drop */
+    /* Drag and Drop - PIXEL SENSE */
+    jQuery(function($){
+        var inside = false;
+        var insideDrop = false;
+
+        $('div#objets ul li div.drag').drag(function( ev, dd ){
+            console.log('drag');
+            if($(this).hasClass('dropped')) return false;
+
+            $(this).css('opacity', .6);
+            // console.log(dd.offsetX);
+            // console.log(dd.offsetY);
+            $(this).css({
+                top: dd.offsetY,
+                left: dd.offsetX
+            });
+        }).drag("end", function() {
+            $(this).css('opacity', 1);
+            console.log("drag end!");
+
+            if(insideDrop) {
+                // if there is an item already dropped
+                if(inside) {
+                    console.log('someone already here!');
+                    old = $('div#objets li div.drag.dropped');
+                    $(old).css({
+                        top: $(old).parent().css('top'),
+                        left: $(old).parent().css('left'),
+                    });
+                    $(old).removeClass('dropped');
+                }
+                
+                $('div#surface div#text').animate({'opacity':'0'});
+                // continue
+                adjustX = ( ($('div#surface div.dropzone').width() - $(this).width()) / 2);
+                adjustY = ( ($('div#surface div.dropzone').height() - $(this).height()) / 2);
+                $(this).css({
+                    left: $('div#surface div.dropzone').position().left + adjustX - 210,
+                    top: $('div#surface div.dropzone').position().top + adjustY,
+                })
+                $(this).addClass('dropped');
+
+                // show text info
+                id = $(this).parent().attr('id');
+                console.log(id);
+                if($('div#surface div.description div').hasClass('active')) {
+                    $('div#surface div.description div').removeClass('active');
+                }
+                $('div#surface div.description div#' + id).addClass('active');
+					
+                inside = true;
+                insideDrop = false;
+            } else {
+                // Back to original pos
+                if($(this).hasClass('dropped')) return false;
+                $(this).css({
+                    top: $(this).parent().css('top'),
+                    left: $(this).parent().css('left'),
+                });
+                console.log($(this).parent().css('top'));
+                console.log('back to zero bitches!');
+            }
+        });
+        $('div#surface div.dropzone')
+            .drop("start",function(){
+                $( this ).addClass("active");
+                console.log('inside!');
+                insideDrop = true;
+            })
+            .drop("end",function(){
+                $( this ).removeClass("active");
+        });
+    });
+            
+    /* Drag and Drop - EDITIONS VOLUMIQUE */
     jQuery(function($){
         var inside = false;
         var insideDrop = false;
@@ -34,12 +118,12 @@
             });
         }).drag("end", function() {
             $(this).css('opacity', 1);
-            console.log("drag end!");
+            // console.log("drag end!");
 
             if(insideDrop) {
                 // if there is an item already dropped
                 if(inside) {
-                    console.log('someone already here!');
+                    // console.log('someone already here!');
                     old = $('ul#pions li div.drag.dropped');
                     $(old).css({
                         top: $(old).parent().css('top'),
@@ -73,7 +157,7 @@
                     top: $(this).parent().css('top'),
                     left: $(this).parent().css('left'),
                 });
-                console.log('back to zero bitches!');
+                // console.log('back to zero bitches!');
             }
         });
         $('div#ipad #dropzone')
